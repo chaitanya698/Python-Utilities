7e2920dd64194b6417615b7d4b96566722e076e7
Chaitanya Annangi <48857925+chaitanya698@users.noreply.github.com>
2025-09-25 17:25:33 +0530

    Delete bdd_tests directory

--- bdd_tests/config/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Configuration package for BDD tests."""
-
-from .loader import get_config, cleanup_config
-from .settings import Settings
-
-__all__ = ['get_config', 'cleanup_config', 'Settings']
